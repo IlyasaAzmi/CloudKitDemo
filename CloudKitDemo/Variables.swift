cc888d86ffafc0ebdcc59717c7426eff20c95fb5
ilyasa Azmi <theazmi99ilyasa@gmail.com>
2019-09-25 17:50:36 +0700

    Initial Commit

---
+++ CloudKitDemo/Variables.swift
@@ -0,0 +1,13 @@
+//
+//  Variables.swift
+//  CloudKitDemo
+//
+//  Created by Ilyasa Azmi on 24/09/19.
+//  Copyright © 2019 Ilyasa Azmi. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+var titles = [String]()
+var recordIDs = [CKRecord.ID]()
